af6965e6da02d11539944fdb160e38c4b2929ccd
nicolediamantino <101394035@georgebrown.ca>
2026-01-25 22:53:24 -0500

    Starting week 02 Lab

---
+++ Week01/lab1.py
@@ -0,0 +1,26 @@
+# Sample Coding Question 01 Week 01
+# Nicole Diamantino
+
+
+# Question 2: Defining an array (list)
+m_array = [1, 4, 7, 9]
+
+# Question 3: Order of Operations
+a = 1
+b = 2
+c = 3
+d = 4
+
+# Fully-Bracked Version:
+e = ((a - ((b ** c) // d)) + (a % c))
+print("question 3: ", e)
+
+# Question 4: Formating
+temperature = 32.6
+print("The temperature today is: {:.3f} degrees Celsius".format(temperature))
+
+# Question 5: User Input
+userAge = int(input("enter your age: "))
+
+userAge = userAge + 22
+print("now showing the shop items filtered by age: ", userAge)
