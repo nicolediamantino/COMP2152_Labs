49c0bb23f47dcab46575a6ba2a28fe1cbe30af02
nicolediamantino <101394035@georgebrown.ca>
2026-02-01 23:32:58 -0500

    Remove Week03 to re-add

--- Week03/Q2.py
+++
@@ -1,8 +0,0 @@
-cart = ["apple", "banana", "milk", "apple", "eggs"]
-apple_count = cart.count("apple")
-print(f"Number of apples: {apple_count}")
-print(f"Position of Milk: {cart.index("milk")}")
-cart.remove("apple")
-print(f"removed item used pop: {cart.pop()}")
-print("is banana in the cart?", "banana" in cart)
-print(f"Final cart: {cart}")
